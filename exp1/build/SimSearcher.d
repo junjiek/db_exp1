fe33f26dcaa65fac87d566d7377ffe11b7e6a551
柯均洁 <kjj3300@163.com>
2015-04-06 08:40:53 +0800

    add refer

---
+++ exp1/build/SimSearcher.d
@@ -0,0 +1,3 @@
+build/SimSearcher.o: src/SimSearcher.cpp src/SimSearcher.h
+
+src/SimSearcher.h:
---
+++ src/SimSearcher.cpp
@@ -0,0 +1,29 @@
+#include "SimSearcher.h"
+
+using namespace std;
+
+SimSearcher::SimSearcher()
+{
+}
+
+SimSearcher::~SimSearcher()
+{
+}
+
+int SimSearcher::createIndex(const char *filename, unsigned q)
+{
+	return SUCCESS;
+}
+
+int SimSearcher::searchJaccard(const char *query, double threshold, vector<pair<unsigned, double> > &result)
+{
+	result.clear();
+	return SUCCESS;
+}
+
+int SimSearcher::searchED(const char *query, unsigned threshold, vector<pair<unsigned, unsigned> > &result)
+{
+	result.clear();
+	return SUCCESS;
+}
+
