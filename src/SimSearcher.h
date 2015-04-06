351c2a2a8a102e3b2edc016b539e450624b1bcb4
柯均洁 <kjj3300@163.com>
2015-04-06 22:49:00 +0800

    add it back...

--- src/SimSearcher.h
+++ src/SimSearcher.h
@@ -1,18 +1,48 @@
 #pragma once
 #include <vector>
 #include <utility>
+#include <cstring>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include "Gram.h"
 
 const int SUCCESS = 0;
 const int FAILURE = 1;
 
-class SimSearcher
-{
-public:
-	SimSearcher();
-	~SimSearcher();
+#define ED 1
+#define JAC 2
 
-	int createIndex(const char *filename, unsigned q);
-	int searchJaccard(const char *query, double threshold, std::vector<std::pair<unsigned, double> > &result);
-	int searchED(const char *query, unsigned threshold, std::vector<std::pair<unsigned, unsigned> > &result);
+using namespace std;
+
+class SimSearcher {
+private:
+    vector<string> _str;
+    unsigned _q, _minGramSize;
+    unordered_map<string, Gram> _map;
+    void generateGram(string &s, unsigned line_num);
+public:
+    SimSearcher() { _minGramSize = INT_MAX; }
+    ~SimSearcher() {}
+    void setQ(int q) { _q = q; }
+    int createIndex(const char *filename, unsigned q);
+    // template <typename TP>
+    int jaccardT(string &query, double threshold);
+    int edT(string &query, unsigned threshold);
+    void generateList(string &query, vector<IList *> &list,
+                      map<int, int> &rawResult, int kind, int T);
+    void scanCount(string &query, vector<IList *> &list,
+                   map<int, int> &rawResult, int T);
+    void divideSkip(string &query, vector<IList *> &list,
+                    map<int, int> &rawResult, int T);
+    void getRawResult(string &query, map<int, int> &rawResult, int kind, int T);
+    unsigned edDist(string &a, string &b, int T, unsigned threshold,
+                    vector<int> &d0, vector<int> &d1);
+    double jaccardDist(string &a, string &b, int T, double threshold,
+                       vector<int> &d0, vector<int> &d1);
+    int searchJaccard(const char *query, double threshold,
+                      std::vector<std::pair<unsigned, double> > &result);
+    int searchED(const char *query, unsigned threshold,
+                 std::vector<std::pair<unsigned, unsigned> > &result);
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,72 @@
 #include "SimSearcher.h"
+#include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s data query\n", argv[0]);
+		return 1;
+	}
+
 	SimSearcher searcher;
 
 	vector<pair<unsigned, unsigned> > resultED;
 	vector<pair<unsigned, double> > resultJaccard;
 
-	unsigned q = 3, edThreshold = 2;
+	unsigned q = 1, edThreshold = 2;
 	double jaccardThreshold = 0.85;
 
 	searcher.createIndex(argv[1], q);
-	searcher.searchJaccard("query", jaccardThreshold, resultJaccard);
-	searcher.searchED("query", edThreshold, resultED);
+	
+	vector<string> strs;
+	FILE *fp = fopen(argv[1], "r");
+	char *line = NULL;
+	size_t len = 0;
+	while (getline(&line, &len, fp) != -1) {
+		line[strlen(line)-1] = '\0';
+		strs.push_back(line);
+	}
+	fclose(fp);
+
+	char buf[2050];
+	fp = fopen(argv[2], "r");
+	clock_t start, end;
+	while (getline(&line, &len, fp) != -1) {
+		if (sscanf(line, "%s\n", buf) != 1)
+			return 2;
+		// ============= Jaccard =============
+		start = clock();
+		searcher.searchJaccard(buf, jaccardThreshold, resultJaccard);
+		end = clock();
+		printf("+ %lf %s (%.2lfs passed, %lu results found)\n",
+				jaccardThreshold, buf, (end-start)/1000.0, resultJaccard.size());
+		for (auto s: resultJaccard)
+			printf("  %lf %s\n", s.second, strs[s.first].c_str());
+
+		// =========== Edit Distance ===========
+		start = clock();
+		searcher.searchED(buf, edThreshold, resultED);
+		end = clock();
+		printf("+ %d %s (%.2lfs passed, %lu results found)\n",
+			edThreshold, buf, (end-start)/1000.0, resultED.size());
+		for (auto s: resultED)
+			printf("  %d %s\n", s.second, strs[s.first].c_str());
+	}
+	fclose(fp);
+
+	// ifstream fin(argv[1]);
+	// string s;
+	// int i = 0;
+	// while(getline(fin, s))
+	// {
+	// 	searcher.searchED(s.c_str(), edThreshold, resultED);
+	// 	cerr << s << endl;
+	// 	cerr << i ++ << endl;
+	// }
+	// searcher.searchED("qu", edThreshold, resultED);
 
 	return 0;
 }
